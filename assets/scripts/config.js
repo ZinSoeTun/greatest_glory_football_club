081fd7b3a387ef2857f985ca2acaa3c8bb90e332
Zin Soe Tun <zinsoetun123@gmail.com>
2026-07-01 12:12:56 +0630

    app_url_api_update_2026_07_01

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,7 +4,7 @@ window.GG_CONFIG = {
   remoteSettingsKey: "ggfc-remote-settings-v1",
   adminModeKey: "ggfc-admin-mode-v1",
   authSessionKey: "ggfc-admin-auth-session-v1",
-  publicApiUrl: "https://script.google.com/macros/s/AKfycbxvnAXqJK0yOMrnXiubPx_2_CNlgCdtrQi8WUL2j_xys36XmQsAO40B16VNzpqElLH52Q/exec",
+  publicApiUrl: "https://script.google.com/macros/s/AKfycbzCUj44-5Xzaq_CjSIwARgPpWEkp6go__8wLfGWj8CH9lTQP0cTmXbGWTfpkwbziVnG/exec",
   maxInlineFileSize: 8 * 1024 * 1024,
   localAdminPasswordHash: "92046c7181b5d3ac1a9f6d2fc88325badd5b7336368c96d5d709ed4b5a766863",
   localSessionHours: 12,
